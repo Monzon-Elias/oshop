80ea3fafb412759866f9e1a6e604005f431a3fa2
elias <https://bitbucket.org/EliosMoncho/repo360/src/master/>
2021-06-15 16:36:15 -0600

    1st commit

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyAnLDjZ8Dw8A8NQzMxahE-FeqpIJuZESis",
+    authDomain: "organic-shop-2e43d.firebaseapp.com",
+    projectId: "organic-shop-2e43d",
+    storageBucket: "organic-shop-2e43d.appspot.com",
+    messagingSenderId: "242986812436",
+    appId: "1:242986812436:web:560bbff11943ced5d65301",
+  },
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyAnLDjZ8Dw8A8NQzMxahE-FeqpIJuZESis",
+    authDomain: "organic-shop-2e43d.firebaseapp.com",
+    projectId: "organic-shop-2e43d",
+    storageBucket: "organic-shop-2e43d.appspot.com",
+    messagingSenderId: "242986812436",
+    appId: "1:242986812436:web:560bbff11943ced5d65301",
+  },
 };
 
 /*
